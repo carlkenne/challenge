3ed19c2156b218d369a3f540540f6ebfd2c2b2f7
Carl Kenne <carl.kenne@gmail.com>
2018-05-06 22:31:24 +0200

    days are now synched with selected year and selected month

--- challenge.js
+++ challenge.js
@@ -1,45 +1,63 @@
 document.addEventListener('DOMContentLoaded', function(event) {
   var state = {
     day: 1,
-    month: 0,
+    month: 1,
     year: moment().year() - 18
   };
+  var component = document.querySelector('.date');
+  var nextBtn = component.querySelector('.date_next-btn');
 
-  initiateSelect('day', 1, 31, 0);
-  initiateSelect('month', 1, 12, -1);
-  initiateSelect('year', 1900, moment().year(), 0);
+  function onMonthOrYearChanged() {
+    var daysInNewMonth = moment({
+      year: state.year,
+      month: state.month - 1
+    }).daysInMonth();
 
-  document
-    .querySelector('.date_next-btn')
-    .addEventListener('click', function() {
-      if (isValid()) {
-        alert(moment(state).format('YYYY-MM-DD'));
-      }
-    });
+    state.day = Math.min(Number(state.day), daysInNewMonth);
+    buildOptions('day', 1, daysInNewMonth);
+  }
 
-  function initiateSelect(type, start, end, valueModifier) {
-    var select = document.querySelector('#date_' + type);
+  initiateSelect('day', 1, 31);
+  initiateSelect('month', 1, 12, onMonthOrYearChanged);
+  initiateSelect('year', 1900, moment().year(), onMonthOrYearChanged);
+
+  nextBtn.addEventListener('click', function() {
+    if (isValid()) {
+      alert(getStateAsMoment().format('YYYY-MM-DD'));
+    }
+  });
+
+  function getSelect(type) {
+    return component.querySelector('#date_' + type);
+  }
+
+  function initiateSelect(type, start, end, onChange) {
+    var select = getSelect(type);
     select.addEventListener('change', function(event) {
       state[type] = event.target.value;
+      onChange && onChange();
       validate();
     });
+    buildOptions(type, start, end);
+  }
 
+  function buildOptions(type, start, end) {
+    var select = getSelect(type);
+    select.innerHTML = '';
     for (var i = start; i <= end; i++) {
-      select.appendChild(createOption(select, i + valueModifier, i));
+      select.appendChild(createOption(select, i));
     }
-
     select.value = state[type];
   }
 
-  function createOption(select, value, innerHTML) {
+  function createOption(select, value) {
     var option = document.createElement('option');
     option.value = value;
-    option.innerHTML = innerHTML;
+    option.innerHTML = value;
     return option;
   }
 
   function validate() {
-    var component = document.querySelector('.date');
     if (isValid()) {
       component.classList.remove('invalid');
     } else {
@@ -48,6 +66,21 @@ document.addEventListener('DOMContentLoaded', function(event) {
   }
 
   function isValid() {
-    return moment.duration({ from: moment(state), to: moment() }).years() >= 18;
+    return (
+      moment
+        .duration({
+          from: getStateAsMoment(),
+          to: moment()
+        })
+        .years() >= 18
+    );
+  }
+
+  function getStateAsMoment() {
+    return moment({
+      day: state.day,
+      month: state.month - 1,
+      year: state.year
+    });
   }
 });
